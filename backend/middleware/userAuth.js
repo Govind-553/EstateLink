eab14a5bd0f3e64a3f634c7938bba196de3aff58
Nishank-C-Jain <nishankjain.study@gmail.com>
2025-08-17 03:22:43 +0530

    The middleware authUser is Updated

--- backend/middleware/userAuth.js
+++ backend/middleware/userAuth.js
@@ -1,42 +1,36 @@
-import jwt from "jsonwebtoken" ; 
+import jwt from "jsonwebtoken";
 
 export const verifyAccessToken = (req, res, next) => {
-    // Access Authorization Header 
-    const authHeader = req.headers.authorization ;
+  const authHeader = req.headers.authorization;
 
-    // Validate Authorization Header
-    if (!authHeader?.startsWith("Bearer ")) {
-        return res.status(401).json({
-            status: "failed",
-            message: "Unauthorized User - No Token!"
-        }) ;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({
+      status: "failed",
+      message: "Unauthorized User - No Token!",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
+
+    if (!decoded) {
+      return res.status(403).json({
+        status: "failed",
+        message: "User Unauthorized - Invalid Token!",
+      });
     }
 
-    // Access Token from Authorization Header
-    const token = authHeader.split(" ")[1] ;
+    req.userId = decoded.userId;
+    req.mobileNumber = decoded.mobileNumber; // works only if included in token payload
 
-    try {
-        // Verify Token
-        const decoded = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET) ;
-
-        // Validate the Verification
-        if (!decoded) {
-            return res.status(403).json({
-                status: "failed",
-                message: "User Unauthorized - Invalid Token!"
-            }) ;
-        }
-
-        // If Token Authenticated then will return the Payload (UserId)
-        // This payload we will set to the req.userId so that it can be used by the next function
-        req.userId = decoded.userId ;
-
-        // Call the Next Function 
-        next() ;
-
-    } catch (error) {
-        console.error("Error in Verifying Token: ", error) ;
-        return res.status(500).json({ message: "Internal Server Error" }) ;
-        
-    }
-} ;
+    next();
+  } catch (error) {
+    console.error("Error in Verifying Token: ", error.message);
+    return res.status(401).json({
+      status: "failed",
+      message: "Invalid or expired token",
+    });
+  }
+};
